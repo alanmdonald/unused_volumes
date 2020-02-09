7564122abf5fec6644b11791f51c31bb3ef478f5
McDonald <alanmdonald@gmail.com>
2020-02-09 13:18:25 +0000

    adding describe-snapshot

--- unused_volumes.py
+++ unused_volumes.py
@@ -5,10 +5,13 @@ client = boto3.client('ec2')
 volumes = client.describe_volumes( Filters=[{'Name': 'status', 'Values': ['available']}])
 
 for volume in volumes['Volumes']:
-    print(volume['Size'])
+    #print(volume['Size'])
     print(volume['VolumeId'])
-    print(volume['Iops'])
-    print(volume['VolumeType'])
+    #print(volume['Iops'])
+    #print(volume['VolumeType'])
+
+    snapshots = client.describe_snapshots( Filters=[{'Name': 'volume-id', 'Values': [volume['VolumeId']]}])
+    print(snapshots)
 
     if volume['VolumeType']=='io1':
         print('volume is io1')
