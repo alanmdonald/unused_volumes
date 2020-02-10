e6cc9c80917a2ff7a2a0f26d5cbe8725baf35d99
EC2 Default User <ec2-user@ip-172-31-4-134.eu-west-1.compute.internal>
2020-02-10 16:43:57 +0000

    update wth tabulate

--- unused_volumes.py
+++ unused_volumes.py
@@ -1,17 +1,29 @@
 import boto3
+from tabulate import tabulate
 
 client = boto3.client('ec2')
 
+table=[]
 volumes = client.describe_volumes( Filters=[{'Name': 'status', 'Values': ['available']}])
 
 for volume in volumes['Volumes']:
     #print(volume['Size'])
-    print(volume['VolumeId'])
+    #print(volume['VolumeId'])
+    #vol=volume['VolumeId']
     #print(volume['Iops'])
     #print(volume['VolumeType'])
+    table.append(volume['VolumeId'])
 
     snapshots = client.describe_snapshots( Filters=[{'Name': 'volume-id', 'Values': [volume['VolumeId']]}])
-    print(snapshots)
+    if not snapshots['Snapshots']:
+        table.append('None')
+    else:
+        table.append(snapshots['Snapshots'][0]['SnapshotId'])
+        #print(snapshots['Snapshots'][0]['SnapshotId']) 
+        #print(snapshots['Snapshots'][0]['StartTime'])
+        #print(type(snapshots['Snapshots']))
 
     if volume['VolumeType']=='io1':
         print('volume is io1')
+
+print(tabulate([[table[0],table[1]], [table[2],table[3]], [table[4],table[5]]], headers=['Volume','Snapshot']))
