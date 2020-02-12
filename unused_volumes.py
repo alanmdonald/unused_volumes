f7858f4b3e36b4fb4d8e7d2cb30246de7dd22cd6
EC2 Default User <ec2-user@ip-172-31-4-134.eu-west-1.compute.internal>
2020-02-12 18:00:01 +0000

    add in region loop

--- unused_volumes.py
+++ unused_volumes.py
@@ -1,33 +1,43 @@
 import boto3
 from tabulate import tabulate
 
-client = boto3.client('ec2', region_name='eu-west-1')
+client = boto3.client('ec2')
 
 table=[]
 volumes = client.describe_volumes( Filters=[{'Name': 'status', 'Values': ['available']}])
 
-for volume in volumes['Volumes']:
-    #print(volume['Size'])
-    #print(volume['VolumeId'])
-    #vol=volume['VolumeId']
-    #print(volume['Iops'])
-    #print(volume['VolumeType'])
-    table.append(volume['VolumeId'])
+for region in client.describe_regions()['Regions']:
 
-    snapshots = client.describe_snapshots( Filters=[{'Name': 'volume-id', 'Values': [volume['VolumeId']]}])
-    if not snapshots['Snapshots']:
-        #table.append('None')
-        snap='None'
-    else:
-        table.append(snapshots['Snapshots'][0]['SnapshotId'])
-        snap=snapshots['Snapshots'][0]['SnapshotId'] 
-        #print(snapshots['Snapshots'][0]['StartTime'])
-        #print(type(snapshots['Snapshots']))
+    regions=region['RegionName']
+    print ("Region: "+ regions)
 
+    client = boto3.client('ec2', region_name=regions)
+    volumes = client.describe_volumes( Filters=[{'Name': 'status', 'Values': ['available']}])
+    snap="None"
+    start="None"
+    for volume in volumes['Volumes']:
+        #print(volume['Size'])
+        #print(volume['VolumeId'])
+        #vol=volume['VolumeId']
+        #print(volume['Iops'])
+        #print(volume['VolumeType'])
+        table.append(volume['VolumeId'])
 
+        snapshots = client.describe_snapshots( Filters=[{'Name': 'volume-id', 'Values': [volume['VolumeId']]}])
+        if not snapshots['Snapshots']:
+            table.append('None')
+            #snap='None'
+            #time='None'
+        else:
+            table.append(snapshots['Snapshots'][0]['SnapshotId'])
+            snap=snapshots['Snapshots'][0]['SnapshotId'] 
+            start=str(snapshots['Snapshots'][0]['StartTime'])
+            #print(type(snapshots['Snapshots']))
+
+        print ("Volume:  " + volume['VolumeId'] + "  Snapshot:  " + snap + "  Creation:  " + start)
    # print(tabulate([[volume['VolumeId'],snap]], headers=['Volume','Snapshot']))
 
-    if volume['VolumeType']=='io1':
-        print('volume is io1')
+    #if volume['VolumeType']=='io1':
+       # print('volume is io1')
 
-print(tabulate([[table[0],table[1]], [table[2],table[3]]], headers=['Volume','Snapshot']))
+#print(tabulate([[table[0],table[1]], [table[2],table[3]]], headers=['Volume','Snapshot']))
